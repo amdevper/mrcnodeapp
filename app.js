24cc205519192505c2010c802a8315ec518a8251
amdevper <68313421+amdevper@users.noreply.github.com>
2021-08-26 07:59:01 +0700

    finalize

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.use(express.static('public'))
 
 app.get('/',(req,res)=>{
     res.render("index",{
-        title:"This is title"
+        title:"This is title",
+        test: "get called"
     })
 })
 
@@ -16,7 +17,58 @@ const server = app.listen(process.env.PORT || 8080,()=>{
 })
 
 const io = socketio(server)
-io.on('connection',()=>{
+/*
+io.on('connection',(socket)=>{
     console.log('Client connected')
+    socket.emit("hello",counter)
+    socket.on('disconnect',()=>{
+        console.log('disconnected')
+    })  
+})
+let counter = 0
+setInterval((socket)=>{
+    console.log('yes',counter)
+    socket.emit("hello",counter)
+    counter++
+},1000)
+*/
+/*
+let counter = 0
+setInterval(()=>{
+    io.on('connection',(sock2)=>{
+        sock2.emit("hello",counter)
+    })
+    counter ++
+},1000)
+*/
+let ddd =0
+const datata = "helllllllooooooowwwwoooorrrrlllld"
+io.on('connection',(socket)=>{
+    console.log('Client connected')
+    socket.emit("hello","Hello from Server")
+    socket.emit("another","another")
+
     
+    setInterval(function() {
+        var currentDate = new Date();
+        
+        io.sockets.emit('clock',{currentDate:currentDate});
+        
+    },1000);
+
+    
+    
+
+    socket.on('disconnect',()=>{
+        console.log('Client leaved')
+    })
+    socket.on('join',(data)=>{
+        console.log('Received from client :',data)
+    })
+    socket.on("toserver",(data)=>{
+        console.log('Received from client :',data)
+    })
+    socket.on('fromclient',(data)=>{
+        console.log('Received from client :',data)
+    })
 })
